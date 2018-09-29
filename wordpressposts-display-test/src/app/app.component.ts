304daf81018edfbd766c2d9ed184797992d41cd2
MiaJohnson <mia.johnson01@gmail.com>
2018-09-29 13:48:42 -0400

    Step 5

--- wordpressposts-display-test/src/app/app.component.ts
+++ wordpressposts-display-test/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { AppService } from './app.service';
-
+import { SafePost } from './safe-post.interface';
 
 @Component({
   selector: 'app-root',
@@ -10,25 +10,21 @@ import { AppService } from './app.service';
 })
 export class AppComponent implements OnInit {
   title = 'Wordpress Display Posts';
-  restItems: any;
-  restItemsUrl = 'https://public-api.wordpress.com/rest/v1.1/sites/vocon-it.com/posts';
+  // restItems: any;
+  // restItemsUrl = 'https://public-api.wordpress.com/rest/v1.1/sites/vocon-it.com/posts';
+  posts: SafePost[];
 
-  constructor(private appService: AppService) {}
+  constructor(private appService: AppService, public sanitizer: DomSanitizer) {}
     
 
   ngOnInit() {
     this.getRestItems();
   }
 
-  // Read all REST Items
-  getRestItems(): void {
+   // Read all REST Items
+   getRestItems(): void {
     this.appService.getAll()
-      .subscribe(
-        restItems => {
-          this.restItems = restItems;
-          console.log(this.restItems);
-        }
-      )
+    .subscribe(posts => { this.posts = posts; });
   }
 
 }
--- wordpressposts-display-test/src/app/app.service.ts
+++ wordpressposts-display-test/src/app/app.service.ts
@@ -1,7 +1,8 @@
 import { Injectable, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
-import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { DomSanitizer } from '@angular/platform-browser';
+import { SafePost } from './safe-post.interface';
 
 @Injectable()
 export class AppService { 
@@ -10,16 +11,28 @@ export class AppService {
 
   constructor(private http: HttpClient, public sanitizer: DomSanitizer) {}
 
-  // Rest Items Service: Read all REST Items
+  safePost(apiDataSinglePost: APIDataSinglePost): SafePost  {
+    return {
+      'title': this.sanitizer.bypassSecurityTrustHtml(apiDataSinglePost.title),
+      'content': this.sanitizer.bypassSecurityTrustHtml(apiDataSinglePost.content)
+    };
+  }
+
+ // Rest Items Service: Read all REST Items
   getAll() {
     return this.http
       .get<any[]>(this.url)
-      .pipe(map(data => data));
+      .pipe(map(apiDataAllPosts => {
+        const mySafePosts: SafePost[] = apiDataAllPosts['posts']
+          .map(apiDataSinglePost =>  this.safePost(apiDataSinglePost));
+        console.log(mySafePosts);
+        return mySafePosts;
+    }));
   }
 
-//   get() {
-//     return this.http
-//       .get<any[]>(this.url)
-//       .pipe(map(data => data));
-//   }
+}
+
+interface APIDataSinglePost {
+  'title': string;
+  'content': string;
 }
